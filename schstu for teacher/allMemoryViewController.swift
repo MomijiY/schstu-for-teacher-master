22696fba60b4a2d08f4b1ac7cf5bd2ab8bc18014
吉川椛 <js20171059@yamanashi-eiwa.ac.jp>
2019-06-20 07:20:52 +0900

    少し改良

--- schstu for teacher/allMemoryViewController.swift
+++ schstu for teacher/allMemoryViewController.swift
@@ -11,14 +11,18 @@ import Firebase
 
 class allMemoryViewController: UIViewController {
     
+    var saveData: UserDefaults = UserDefaults.standard
+    
     var database: Firestore!
+    
+    @IBOutlet weak var nameLabel: UILabel!
 
     @IBOutlet weak var Rememory: UIButton!
     
     @IBOutlet weak var timeLabel: UILabel!
-//
+
     @IBOutlet weak var subjectLabel: UILabel!
-//
+
     @IBOutlet weak var pageLabel: UILabel!
     
     override func viewDidLoad() {
@@ -56,34 +60,11 @@ class allMemoryViewController: UIViewController {
             
             self.pageLabel.text = data["page"] as? String
    
-//    @IBAction func RememoryAc(_ sender: Any) {
-//        database.collection("data").document("example").getDocument { (snap, error) in
-//            if let error = error {
-//                fatalError("\(error)")
-//            }
-//            guard let data = snap?.data() else { return }
-//            print(data["text"]!)
-//
-//            self.timeLabel.text = data["text"] as? String
-    }
-//        database.collection("subject").document("example").getDocument { (snap, error) in
-//            if let error = error {
-//                fatalError("\(error)")
-//            }
-//            guard let data = snap?.data() else { return }
-//            print(data["Subject"]!)
-//
-//            self.subjectLaebl.text = data["Subject"] as? String
-//    }
-//        database.collection("page").document("example").getDocument { (snap, error) in
-//            if let error = error {
-//                fatalError("\(error)")
-//            }
-//            guard let data = snap?.data() else { return }
-//            print(data["Page"]!)
-//
-//            self.pageLabel.text = data["Page"] as? String
-//    }
-
-}
+            }
+        
+        saveData.set(timeLabel.text, forKey: "Text")
+        saveData.set(pageLabel.text, forKey: "Page")
+        saveData.set(subjectLabel.text, forKey: "Subject")
+   }
+    
 }
